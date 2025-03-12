141ff0a7b4e4a561764f40d6231d575914a500ef
Ramin <raminramazanpour@gmail.com>
2025-03-12 12:50:00 +0330

    add anthropicGenerateText

--- packages/plugin-giveth-db/src/helpers/helpers.ts
+++ packages/plugin-giveth-db/src/helpers/helpers.ts
@@ -1,10 +1,11 @@
 import {AppDataSource} from "./ormconfig.ts";
+const { ANTHROPIC_API } = process.env;
 
 export const cleanSqlQuery = (rawSqlQuery: string): string => {
-    return rawSqlQuery.replace(/```sql|```/g, '').trim();
+    return rawSqlQuery?.replace(/```sql|```/g, '').trim();
 }
 
-export async function queryRawSql(sql: string, parameters?: any[]) {
+export const queryGivethDB = async (sql: string, parameters?: any[]) => {
     try {
         await AppDataSource.initialize();
         const result = await AppDataSource.query(sql, parameters);
@@ -15,4 +16,39 @@ export async function queryRawSql(sql: string, parameters?: any[]) {
         await AppDataSource.destroy();
         throw error;
     }
+}
+
+export const anthropicGenerateText = async (context: string) => {
+    const myHeaders = new Headers();
+    myHeaders.append("x-api-key", ANTHROPIC_API);
+    myHeaders.append("anthropic-version", "2023-06-01");
+    myHeaders.append("content-type", "application/json");
+
+    const raw = JSON.stringify({
+        "model": "claude-3-7-sonnet-20250219",
+        "max_tokens": 64000,
+        "messages": [
+            {
+                "role": "user",
+                "content": context
+            }
+        ]
+    });
+
+    const requestOptions: RequestInit = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    const res = await fetch("https://api.anthropic.com/v1/messages", requestOptions)
+    const data = await res.json();
+    console.log('------------------')
+    console.log('context:', context);
+    console.log('------------------')
+    console.log('------------------')
+    console.log('anthropicRes:', data);
+    console.log('------------------')
+    return data?.content[0]?.text;
 }
